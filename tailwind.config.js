1c2e3f3dc7cfea5b3888502aa6406d56b879262c
DMTMVLVAKlN <48865716+DMTMVLVAKlN@users.noreply.github.com>
2025-11-01 05:11:06 +0300

    Update and rename tailwind.config.ts to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
-import type { Config } from 'tailwindcss'
-export default {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: ['./app/**/*.{ts,tsx}', './components/**/*.{ts,tsx}'],
   theme: {
     extend: {
@@ -15,4 +15,4 @@ export default {
     }
   },
   plugins: []
-} satisfies Config
+};
